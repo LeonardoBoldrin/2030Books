cf2fdc6369dc6f5287553408cdb05d645326bdba
LeonardoBoldrin <897157@stud.unive.it>
2024-11-09 19:04:37 +0100

    dovrebbe funzionare ma devo provare

--- app/src/main/java/com/example/a2030books/ButtonsActivity.java
+++ app/src/main/java/com/example/a2030books/ButtonsActivity.java
@@ -8,11 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class ButtonsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // https://a2030books-default-rtdb.europe-west1.firebasedatabase.app
+        
 
 
     }
